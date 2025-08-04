1af1a4bbf66e1bfe87b23d6b2f14caf8a4f04ae6
SwitU7Ronald <c2tu6u@gmail.com>
2025-08-04 16:13:54 +0530

    Entry Of ToDo App

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home_screens.dart';
 
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: ("ToDo App"),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: HomeScreens(),
+    );
+  }
+}
